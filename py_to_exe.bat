935993fc1f472bb2dac12ec420741bfbb2120272
carles <selrak14@gmail.com>
2023-06-05 16:40:01 +0200

    Create py_to_exe.bat

---
+++ py_to_exe.bat
@@ -0,0 +1,1 @@
+pyinstaller app.py --workpath "../Ejecutable_EXE/tmp" --distpath "../Ejecutable_EXE/dist" --clean  --add-data "./modules";"./modules" --add-data "./colormanagement";"./colormanagement" 
