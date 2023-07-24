a2b5abef664d29e2589abbe6ecefd3bd99188b4f
Carles <carlesmatosesg@gmail.com>
2023-07-24 20:25:57 +0200

    Update py_to_exe.bat

--- py_to_exe.bat
+++ py_to_exe.bat
@@ -1,1 +1,1 @@
-pyinstaller app.py --workpath "../Ejecutable_EXE/tmp" --distpath "../Ejecutable_EXE/dist" --clean  --add-data "./modules";"./modules" --add-data "./colormanagement";"./colormanagement" 
+pyinstaller app.py --workpath "../aplicacion-exe_tmp/tmp" --distpath "../aplicacion-exe/dist" --clean  --add-data "./modules";"./modules" --add-data "./colormanagement";"./colormanagement" 
